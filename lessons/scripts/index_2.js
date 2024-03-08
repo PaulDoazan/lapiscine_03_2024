ae6d3cefbeea69dfb77512240f7248a1781ba1f6
pdoazan <paul.doazan@gmail.com>
2024-03-08 10:09:25 +0100

    START DAY 2

---
+++ lessons/scripts/index_2.js
@@ -0,0 +1,19 @@
+let person1 = {
+    name: 'Max',
+    vehicle: 'car',
+    town: 'Paris'
+}
+
+let person2 = {
+    name: 'Nathalie',
+    vehicle: 'bike',
+    town: 'Bordeaux'
+}
+
+let person3 = {
+    name: 'Cathy',
+    vehicle: 'car',
+    town: 'Lyon'
+}
+
+// écrire la condition pour afficher "{Un tel} a une voiture"
