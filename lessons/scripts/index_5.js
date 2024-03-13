8c9a7ff01db6e1b7e1134d4c758eca8241681187
pdoazan <paul.doazan@gmail.com>
2024-03-13 15:19:48 +0100

    Add user-select none css rule on body element

--- lessons/scripts/index_5.js
+++ lessons/scripts/index_5.js
@@ -6,37 +6,13 @@ const refreshDiv = document.querySelector('.refresh')
 
 let correctResult = 0
 
-for (let i = 0; i < numberDivs.length; i++) {
-    const random = Math.round(Math.random() * 10)
-    numberDivs[i].textContent = random
-
-    // Calcul du résultat
-    correctResult += random
-}
-
-// On remplace la valeur des btns par la valeur correcte à laquelle on ajoute/soustrait une marge d'erreur
-// 1. On définit de façon aléatoire la marge d'erreur entre 1 et 5
-// 2. On soustrait/ajoute cette marge d'erreur à correctResult et on l'affecte au textContent de chaque bouton
-let randomMargin = getRandomBetweenNumbers(1, 2)
-
-// Ajouter un écouteur d'événement de type "click" et dans la fonction de callback, afficher en console la valeur du bouton
 for (let i = 0; i < btnDivs.length; i++) {
     btnDivs[i].addEventListener("click", onBtnClick)
-
-    if (Math.random() < 0.5) {
-        btnDivs[i].textContent = correctResult + randomMargin
-    } else {
-        btnDivs[i].textContent = correctResult - randomMargin
-    }
-
-    // On recalcule une marge aléatoire
-    randomMargin = getRandomBetweenNumbers(randomMargin + 1, randomMargin + 2)
 }
 
-let randomPosition = Math.round(Math.random() * (btnDivs.length - 1))
-btnDivs[randomPosition].textContent = correctResult
+// on déclare l'event listener sur le bouton refresh
 
-resultDiv.textContent = "..."
+init()
 
 // Implémenter la fonction callback onBtnClick, avec le test du résultat correct, et l'affichage dans la balise result
 function onBtnClick(e) {
@@ -51,4 +27,36 @@ function onBtnClick(e) {
 
 function getRandomBetweenNumbers(min, max) {
     return Math.round((Math.random() * (max - min)) + min)
+}
+
+function init() {
+    for (let i = 0; i < numberDivs.length; i++) {
+        const random = Math.round(Math.random() * 10)
+        numberDivs[i].textContent = random
+
+        // Calcul du résultat
+        correctResult += random
+    }
+
+    // On remplace la valeur des btns par la valeur correcte à laquelle on ajoute/soustrait une marge d'erreur
+    // 1. On définit de façon aléatoire la marge d'erreur entre 1 et 5
+    // 2. On soustrait/ajoute cette marge d'erreur à correctResult et on l'affecte au textContent de chaque bouton
+    let randomMargin = getRandomBetweenNumbers(1, 2)
+
+    // Ajouter un écouteur d'événement de type "click" et dans la fonction de callback, afficher en console la valeur du bouton
+    for (let i = 0; i < btnDivs.length; i++) {
+        if (Math.random() < 0.5) {
+            btnDivs[i].textContent = correctResult + randomMargin
+        } else {
+            btnDivs[i].textContent = correctResult - randomMargin
+        }
+
+        // On recalcule une marge aléatoire
+        randomMargin = getRandomBetweenNumbers(randomMargin + 1, randomMargin + 2)
+    }
+
+    let randomPosition = Math.round(Math.random() * (btnDivs.length - 1))
+    btnDivs[randomPosition].textContent = correctResult
+
+    resultDiv.textContent = "..."
 }
