35f127fc38a2ec762a2364fbe3b3c472c653cc5e
pdoazan <paul.doazan@gmail.com>
2024-03-13 12:12:11 +0100

    Add getRandomBetweenNumbers function

--- lessons/scripts/index_5.js
+++ lessons/scripts/index_5.js
@@ -3,18 +3,21 @@ const numberDivs = document.querySelectorAll('.number')
 const btnDivs = document.querySelectorAll('.btn')
 const resultDiv = document.querySelector('.result')
 
-let randomNumbers = []
 let correctResult = 0
 
 for (let i = 0; i < numberDivs.length; i++) {
     const random = Math.round(Math.random() * 20)
-    // La méthode .push() d'un tableau permet d'ajouter un élément à la fin du tableau
-    randomNumbers.push(random)
     numberDivs[i].textContent = random
 
     // Calcul du résultat
     correctResult += random
 }
+
+// On remplace la valeur des btns par la valeur correcte à laquelle on ajoute/soustrait une marge d'erreur
+// 1. On définit de façon aléatoire la marge d'erreur entre 1 et 5
+// 2. On soustrait/ajoute cette marge d'erreur à correctResult et on l'affecte au textContent de chaque bouton
+
+let randomMargin = getRandomBetweenNumbers(1, 5)
 
 for (let i = 0; i < btnDivs.length; i++) {
     btnDivs[i].textContent = Math.round(Math.random() * 20)
@@ -24,3 +27,8 @@ let randomPosition = Math.round(Math.random() * (btnDivs.length - 1))
 btnDivs[randomPosition].textContent = correctResult
 
 resultDiv.textContent = "..."
+
+
+function getRandomBetweenNumbers(min, max) {
+    return Math.round((Math.random() * (max - min)) + min)
+}
