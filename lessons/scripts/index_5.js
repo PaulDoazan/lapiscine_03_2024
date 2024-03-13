cc768d7ec2705fe9b02eb118df5cabd3ac3c9d39
pdoazan <paul.doazan@gmail.com>
2024-03-13 11:24:51 +0100

    Update mental calculation app

--- lessons/scripts/index_5.js
+++ lessons/scripts/index_5.js
@@ -1,1 +1,26 @@
-// Récupérer les éléments du dom number, btn, result, et les rafraîchir =>  0, ...
+// Récupérer les éléments du dom : number, btn, result, et les rafraîchir =>  0, ...
+const numberDivs = document.querySelectorAll('.number')
+const btnDivs = document.querySelectorAll('.btn')
+const resultDiv = document.querySelector('.result')
+
+let randomNumbers = []
+let correctResult = 0
+
+for (let i = 0; i < numberDivs.length; i++) {
+    const random = Math.round(Math.random() * 20)
+    // La méthode .push() d'un tableau permet d'ajouter un élément à la fin du tableau
+    randomNumbers.push(random)
+    numberDivs[i].textContent = random
+
+    // Calcul du résultat
+    correctResult += random
+}
+
+for (let i = 0; i < btnDivs.length; i++) {
+    btnDivs[i].textContent = Math.round(Math.random() * 20)
+}
+
+let randomPosition = Math.round(Math.random() * (btnDivs.length - 1))
+btnDivs[randomPosition].textContent = correctResult
+
+resultDiv.textContent = "..."
