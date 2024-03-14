adee920b9ca9f669a60ffcf248d15802d39670be
pdoazan <paul.doazan@gmail.com>
2024-03-14 17:21:51 +0100

    Update align 4 game

--- exercices/damier/index.js
+++ exercices/damier/index.js
@@ -19,6 +19,8 @@ const nbRow = 5
 
 const grid = []
 
+let clickIsRed = true
+
 for (let i = 0; i < nbRow; i++) {
     const row = document.createElement('div')
     row.className = "row"
@@ -38,7 +40,10 @@ console.log(grid)
 // grid[2][4].style.backgroundColor = "yellow"
 
 // Implémenter un puissance 4
-// Chaque case est cliquable
+
+// Chaque case est cliquable => console.log("click")
+
+
 // Quand on clique, on alterne entre couleur jaune et couleur rouge
 // Une case cliquée n'est plus cliquable
 // On peut cliquer sur une case en l'air
@@ -48,9 +53,31 @@ function createSquare(parentNode) {
     square.className = "squareSize"
     const innerSquare = document.createElement('div')
     innerSquare.className = "innerCircle"
+
+    innerSquare.addEventListener('click', onInnerSquareClick)
+
     square.appendChild(innerSquare)
-    square.style.backgroundColor = "blue"
+    square.style.backgroundColor = "#2962ff"
     parentNode.appendChild(square)
 
     return square
+}
+
+function onInnerSquareClick(event) {
+    if (event.currentTarget.isClicked) {
+        return
+    }
+
+    // Condition ternaire
+    event.currentTarget.style.backgroundColor = clickIsRed ? "red" : "yellow"
+
+    // if (clickIsRed) {
+    //     event.currentTarget.style.backgroundColor = "red"
+    // } else {
+    //     event.currentTarget.style.backgroundColor = "yellow"
+    // }
+
+    event.currentTarget.isClicked = true
+
+    clickIsRed = !clickIsRed
 }
