4e25a32658ff18aba2f90683ea75f3bf1aaab8b6
pdoazan <paul.doazan@gmail.com>
2024-03-14 10:52:39 +0100

    Extract data as json

--- exercices/scripts/index_a.js
+++ exercices/scripts/index_a.js
@@ -1,50 +1,15 @@
-const pizzas = [
-    {
-        name: 'Margherita',
-        price: 11.50,
-        ingredients: ['mozzarella', 'tomate', 'basilic ', "huile d'olive"],
-        baseTomate: true
-    },
-    {
-        name: 'Regina',
-        price: 12,
-        ingredients: ['mozzarella', 'tomate', 'origan', 'jambon', 'champignons'],
-        baseTomate: true
-    },
-    {
-        name: '4 saisons',
-        price: 15,
-        ingredients: ['artichaut', 'courgette', 'poivron', 'mozzarella', 'oignon rouge'],
-        baseTomate: true
-    },
-    {
-        name: 'Napolitaine',
-        price: 14,
-        ingredients: ['tomate', 'mozzarella', 'anchois', 'olives noires', 'origan'],
-        baseTomate: true
-    },
-    {
-        name: '4 fromages',
-        price: 16,
-        ingredients: ['tomate', 'mozzarella', 'emmental', 'comté', 'roquefort'],
-        baseTomate: true
-    },
-    {
-        name: 'Montagnarde',
-        price: 19,
-        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
-        baseTomate: false
-    },
-    {
-        name: 'Chèvre-miel',
-        price: 18,
-        ingredients: ['mozzarella', 'chèvre', 'miel'],
-        baseTomate: false
-    },
-    {
-        name: 'Hawaïenne',
-        price: 17,
-        ingredients: ['mozzarella', 'tomate', 'jambon', 'ananas'],
-        baseTomate: true
+import pizzas from "../data/pizzas.json" assert {type: "json"}
+
+// Afficher dans la console une chaîne de caractère de tous les ingrédients de la dernière pizza : "mozzarella, tomate..."
+let result = ""
+const ingredientsArray = pizzas[pizzas.length - 1].ingredients
+
+for (let i = 0; i < ingredientsArray.length; i++) {
+    if (i !== ingredientsArray.length - 1) {
+        result += ingredientsArray[i] + ", "
+    } else {
+        result += ingredientsArray[i] + " !"
     }
-]
+}
+
+console.log(result)
