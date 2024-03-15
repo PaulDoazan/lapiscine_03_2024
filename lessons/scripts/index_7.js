a2a941cd38ddfcd61a7b4ad0b1d1eb20c5287a7d
pdoazan <paul.doazan@gmail.com>
2024-03-15 10:00:31 +0100

    Refresh on dom interaction

--- lessons/scripts/index_7.js
+++ lessons/scripts/index_7.js
@@ -1,1 +1,14 @@
 console.log("start day 7")
+
+// const body = document.querySelector('body')
+const container = document.querySelector('.container')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener("mousedown", onContainerDown)
+
+function onContainerDown(e) {
+    const myDiv = document.createElement("div")
+    myDiv.className = "circle"
+    myDiv.textContent = "Hello world"
+    container.appendChild(myDiv)
+}
