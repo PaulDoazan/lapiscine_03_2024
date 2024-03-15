ee05b6af7da63bac2f50412da7fd6c5a854b4405
pdoazan <paul.doazan@gmail.com>
2024-03-15 15:35:24 +0100

    Introduce .map() Array method

--- exercices/scripts/index_a.js
+++ exercices/scripts/index_a.js
@@ -50,3 +50,103 @@ function getUltimatePizza(arr1, arr2) {
 
     return getMostExpensivePizza(totalArray)
 }
+
+
+// Ecrire une fonction qui prend en paramète un tableau de pizzas, et qui me retourne le prix moyen de toutes les pizzas
+const resultAvg = getAveragePrice(pizzas)
+// console.log(resultAvg)
+
+function getAveragePrice(arr) {
+    let total = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i].price
+    }
+
+    const average = total / arr.length
+
+    // Méthode alternative pour tronquer un nombre après la virgule
+    return average.toFixed(2)
+}
+// Ecrire une fonction qui prend 2 paramètres : un tableau de pizza et un nom de pizza. Cette fonction retourne un tableau des ingrédients de cette pizza
+const resultIngredients = getIngredientsByName(pizzas, "Regina")
+// console.log(resultIngredients)
+
+function getIngredientsByName(arr, nameParam) {
+    let ingredientsArr
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === nameParam) {
+            ingredientsArr = arr[i].ingredients
+            // Une fois l'element trouve, il est inutile de continuer a executer la boucle
+            break
+        }
+    }
+    return ingredientsArr
+}
+
+// Ecrire une fonction qui prend en parametre un tableau de pizzas, et qui retourne un tableau des noms de pizzas qui sont a base de tomate
+const resultNames = getPizzasName(pizzas)
+// console.log(resultNames)
+
+
+
+// Amélioration, la fonction prend en 2ème paramètre un tableau de noms de pizzas, et retourne un tableau des ingrédients COMMUNS à ces pizzas
+
+
+
+function getPizzasNameWithTomato(arr) {
+    let pizzaNames = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].baseTomate) pizzaNames.push(arr[i].name)
+    }
+    return pizzaNames
+}
+
+function getPizzasName(arr) {
+    let pizzaNames = []
+
+    for (let i = 0; i < arr.length; i++) {
+        pizzaNames.push(arr[i].name)
+    }
+
+    return pizzaNames
+}
+
+// NOUVELLE SYNTAXE DES FONCTIONS : LES FONCTIONS FLECHEES, ARROW FUNCTIONS
+const container = document.querySelector('.container')
+
+// fonction anonyme
+const myFunction = (e) => {
+    console.log(e.currentTarget)
+}
+
+container.addEventListener('click', () => { })
+
+const names = pizzas.map((element) => {
+    let result
+    if (element.baseTomate) {
+        result = "Pizza a la tomate : " + element.name
+    } else {
+        result = "Pizza a la creme fraiche : " + element.name
+    }
+
+    return result
+})
+
+// console.log(pizzas.map(el => el.name))
+console.log(pizzas.map(el => el.baseTomate ? "Pizza a la tomate : " + el.name : "Pizza a la creme fraiche : " + el.name))
+// console.log(names)
+
+const numbers = [2, 5, 34, 32, 12]
+const doubles = numbers.map((number) => {
+    return number * 2
+})
+
+console.log(doubles)
+
+// a l'aide d'un .map, afficher un tableau qui contient tous les doubles de numbers
+
+
+
+
