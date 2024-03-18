471661ed9fbb2f71bc88e9dba2995b4f37cfa25b
pdoazan <paul.doazan@gmail.com>
2024-03-18 12:14:24 +0100

    Add exercice index_b.js

---
+++ exercices/scripts/index_b.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//     console.log("index_b.js")
+// }, 3000)
+
+import movies from '../data/movies.json' assert {type: "json"}
+console.log(movies)
+
+// a. Logger le premier element du tableau movies
+// b. Logger l'annee du 4eme element du tableau movies
+// c. Logger le titre du dernier element du tableau movies
+
+// écrire les scripts dans des fonctions
+
+// d. Logger le titre du film qui a la meilleure note
+// e. Logger le titre du film le plus ancien
+// f. Logger un tableau de tous les titres de film qui ont au moins 3 acteurs
+
+// 1. Ecrire une fonction qui prend en parametre le tableau movies et qui retourne un nouveau tableau de tous les titres de films
+// 2. Ecrire une fonction qui prend en parametre une annee et un tableau (movies), et qui retourne un nouveau tableau de titres de film de l'annee specifiee
+// 3. Ecrire une fonction qui prend en parametre un realisateur et un tableau (movies), et qui retourne un nouveau tableau de tous les titres de films de ce realisateur
+
