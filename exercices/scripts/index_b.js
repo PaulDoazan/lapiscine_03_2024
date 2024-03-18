56e9a1f11a8db86fb0a80b4abd308b86e6266ab0
pdoazan <paul.doazan@gmail.com>
2024-03-18 12:46:15 +0100

    WUP correct movies exercice

--- exercices/scripts/index_b.js
+++ exercices/scripts/index_b.js
@@ -3,15 +3,34 @@
 // }, 3000)
 
 import movies from '../data/movies.json' assert {type: "json"}
-console.log(movies)
+// console.log(movies)
 
 // a. Logger le premier element du tableau movies
+// console.log(movies[0])
+
 // b. Logger l'annee du 4eme element du tableau movies
+// console.log(movies[3].year)
+
 // c. Logger le titre du dernier element du tableau movies
+// console.log(movies[movies.length - 1].title)
 
 // écrire les scripts dans des fonctions
 
 // d. Logger le titre du film qui a la meilleure note
+const bestMovie = getBestMovie(movies)
+console.log(bestMovie.title)
+
+// difficulté supplémentaire : on peut utiliser la méthode de tableau .sort()
+function getBestMovie(arr) {
+    let best = arr[0]
+    arr.forEach((element) => {
+        if (element.rate > best.rate) {
+            best = element
+        }
+    })
+    return best
+}
+
 // e. Logger le titre du film le plus ancien
 // f. Logger un tableau de tous les titres de film qui ont au moins 3 acteurs
 
