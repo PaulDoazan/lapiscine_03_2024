af91cf2baf13910c44f7c393cbb52a1f14537155
pdoazan <paul.doazan@gmail.com>
2024-03-19 16:40:49 +0100

    Correct exo 2 in DOM_interaction

--- exercices/DOM_interaction/starter/src/main.js
+++ exercices/DOM_interaction/starter/src/main.js
@@ -4,7 +4,25 @@ const divToColor_1 = document.querySelector('.div-to-color-1')
 
 btns_1.forEach(btn => {
     btn.addEventListener('click', (e) => {
+        console.log(e.target, e.currentTarget)
         divToColor_1.style.backgroundColor = e.currentTarget.textContent
     })
 })
 
+
+// EXO 2
+const textToDisplay3 = document.querySelector('.text-to-display-3')
+const input3 = document.querySelector('.input-3')
+// type d'event => "input"
+
+input3.addEventListener('input', (e) => {
+    // e.target est la balise sur laquelle l'utilisateur a déclenché l'event (sur laquelle il a cliqué par ex)
+    // e.currentTarget est la balise sur laquelle a été déclaré le .addEventListener()
+    // Dans la plupart des cas, c'est la même balise. Mais si une balise contient une balise enfant, e.target sera l'enfant et e.currentTarget le parent. 
+    console.log(e.target, e.currentTarget)
+    if (!e.target.value) {
+        textToDisplay3.textContent = "no text"
+        return
+    }
+    textToDisplay3.textContent = e.target.value
+})
