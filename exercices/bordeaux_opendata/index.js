bb3cc67b0a7a91aa0e32c8bae69a3ca65af59854
pdoazan <paul.doazan@gmail.com>
2024-03-19 12:07:44 +0100

    Add button interaction on FetchData

--- exercices/bordeaux_opendata/index.js
+++ exercices/bordeaux_opendata/index.js
@@ -2,17 +2,39 @@
 
 // création de div
 const mainInterface = document.querySelector('.main-interface')
-const div = document.createElement('div')
-div.innerHTML = `<div class="note">JS DOM</div>`
-mainInterface.appendChild(div)
+const button = document.querySelector('button')
+
+button.addEventListener("click", onBtnClick)
+
+// const div = document.createElement('div')
+// div.innerHTML = `<div class="note">HELLO WORLD</div>`
+// mainInterface.appendChild(div)
+
+function onBtnClick() {
+    btnChange("search")
+    fetchData()
+}
+
+function btnChange(btnState) {
+    if (btnState === "search") {
+        button.textContent = "..."
+        button.classList.add('searching')
+    } else {
+        button.textContent = "Actualiser"
+        button.classList.remove('searching')
+    }
+
+}
 
 async function fetchData() {
     try {
         const response = await fetch("https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/bor_frequentation_piscine_tr/records?limit=20");
         const data = await response.json();
 
+        btnChange("refresh")
         displayData(data.results)
     } catch (error) {
+        btnChange("refresh")
         console.log(error)
     }
 }
