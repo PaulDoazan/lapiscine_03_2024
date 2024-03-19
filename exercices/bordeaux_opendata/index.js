4c629a4dab304f9acab728164eb625060534b5f3
pdoazan <paul.doazan@gmail.com>
2024-03-19 11:18:12 +0100

    Fix html with document.createElement

--- exercices/bordeaux_opendata/index.js
+++ exercices/bordeaux_opendata/index.js
@@ -1,3 +1,11 @@
+// Implémenter un bouton Actualiser pour consommer l'API bordeaux opendata
+
+// création de div
+const mainInterface = document.querySelector('.main-interface')
+const div = document.createElement('div')
+div.innerHTML = `<div class="note">JS DOM</div>`
+mainInterface.appendChild(div)
+
 async function fetchData() {
     try {
         const response = await fetch("https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/bor_frequentation_piscine_tr/records?limit=20");
