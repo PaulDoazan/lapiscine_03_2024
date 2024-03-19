87695035bf89070f75d64b394b6ebdd2a5820507
pdoazan <paul.doazan@gmail.com>
2024-03-19 12:27:22 +0100

    Update html template when create div

--- exercices/bordeaux_opendata/index.js
+++ exercices/bordeaux_opendata/index.js
@@ -2,13 +2,10 @@
 
 // création de div
 const mainInterface = document.querySelector('.main-interface')
+const poolsContainer = document.querySelector('.pools-container')
 const button = document.querySelector('button')
 
 button.addEventListener("click", onBtnClick)
-
-// const div = document.createElement('div')
-// div.innerHTML = `<div class="note">HELLO WORLD</div>`
-// mainInterface.appendChild(div)
 
 function onBtnClick() {
     btnChange("search")
@@ -40,5 +37,23 @@ async function fetchData() {
 }
 
 function displayData(pools) {
+    // on rafraîchit le poolsContainer
+    poolsContainer.innerHTML = ""
+    pools.forEach(pool => {
+        console.log(pool)
+        const div = document.createElement('div')
+        //<div class="place" data-index="0">
+        div.classList.add("place")
+        div.innerHTML = `
+        <div class="place-name">nom de la piscine</div>
+        <div class="place-infos">... / ...</div>
+        <div class="diagram-container">
+          <div class="capacity"></div>
+          <div class="visitors"></div>
+        </div>
+        `
+        poolsContainer.appendChild(div)
+    });
+
     console.log(pools)
 }
