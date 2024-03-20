b472719fa7bcd0345df3a85c360b59d8a5749b2f
pdoazan <paul.doazan@gmail.com>
2024-03-20 11:57:17 +0100

    Preview on destructuring

--- lessons/scripts/index_10.js
+++ lessons/scripts/index_10.js
@@ -14,3 +14,26 @@ class Car {
 
 let berlingo = new Car("Berlingo", "Citroën", 2014)
 let kangoo = new Car("Kangoo", "Renault", 2012)
+
+
+// Destructuration de données, DESTRUCTURING
+
+let person = {
+    firstname: "Paul",
+    lastname: "Doazan",
+    age: 35
+}
+
+// let firstname = person.firstname
+// let lastname = person.lastname
+// let age = person.age
+
+let { firstname } = person
+firstname += "ine"
+
+console.log(firstname)
+
+const numbers = [1, 2, 3, 4, 5]
+
+const [nb1, nb2, ...restNumbers] = numbers
+console.log(restNumbers);
